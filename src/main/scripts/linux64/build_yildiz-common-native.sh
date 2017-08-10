0eb36573b02627e5908af12a1e3a53a6684bc04e
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2017-08-10 12:38:50 +0200

    [NO-JIRA] Initial commit.

---
+++ src/main/scripts/linux64/build_yildiz-common-native.sh
@@ -0,0 +1,8 @@
+cmake . -DCMAKE_MODULE_PATH=. -DCMAKE_BUILD_TYPE=Release -DLIBRARY_OUTPUT_PATH="../../../../target/classes/linux64" -G "Unix Makefiles"
+
+make
+
+rm -R CMakeFiles
+rm CMakeCache.txt
+rm cmake_install.cmake
+rm Makefile
