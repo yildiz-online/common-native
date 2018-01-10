c572bae31bde598b5856cfa917637267f3771b8e
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2018-01-10 17:05:08 +0100

    [YCN-0] Use commons.

---
+++ src/main/java/be/yildizgames/common/nativeresources/OperatingSystem.java
@@ -0,0 +1,63 @@
+/*
+ * This file is part of the Yildiz-Engine project, licenced under the MIT License  (MIT)
+ *
+ *  Copyright (c) 2017 Grégory Van den Borre
+ *
+ *  More infos available: https://www.yildiz-games.be
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ *  documentation files (the "Software"), to deal in the Software without restriction, including without
+ *  limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ *  of the Software, and to permit persons to whom the Software is furnished to do so,
+ *  subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all copies or substantial
+ *  portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ *  WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS
+ *  OR COPYRIGHT  HOLDERS BE LIABLE FOR ANY CLAIM,
+ *  DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE  SOFTWARE.
+ *
+ */
+
+package be.yildizgames.common.nativeresources;
+
+/**
+ * Possible different system with different files.
+ *
+ * @author Grégory Van den Borre
+ */
+public enum OperatingSystem {
+
+    /**
+     * Windows 64 bits.
+     */
+    WIN64("win64"),
+
+    /**
+     * Linux 64 bits.
+     */
+    LINUX64("linux64");
+
+    /**
+     * Directory to hold the file for this particular system.
+     */
+    private final String path;
+
+    OperatingSystem(final String path) {
+        this.path = path;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public static OperatingSystem getCurrent() {
+        if(NativeUtil.isLinux()) {
+            return LINUX64;
+        }
+        return WIN64;
+    }
+}
